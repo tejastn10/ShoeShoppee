6e1e1510353f20b29403ea03a2bcba7690f933b8
Tejas Nikhar <tejastn10@gmail.com>
2020-12-20 14:48:04 +0530

    📡 INITIALIZE: Server

---
+++ backend/dist/app.js
@@ -0,0 +1,30 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importStar(require("express"));
+const dotenv_1 = require("dotenv");
+dotenv_1.config();
+const app = express_1.default();
+app.use(express_1.json());
+app.get("/", (_req, res) => res.send("API Running on Port 5000"));
+const PORT = process.env.PORT || 5000;
+const ENV = process.env.NODE_ENV || 5000;
+app.listen(PORT, () => console.log(`Backend server running in ${ENV} mode on port ${PORT}`));
---
+++ backend/src/app.ts
@@ -0,0 +1,18 @@
+import express, { json, Application, Request, Response } from "express";
+import { config } from "dotenv";
+
+config();
+const app: Application = express();
+
+app.use(json());
+
+app.get("/", (_req: Request, res: Response) =>
+  res.send("API Running on Port 5000")
+);
+
+const PORT: string | number = process.env.PORT || 5000;
+const ENV: string | number = process.env.NODE_ENV || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Backend server running in ${ENV} mode on port ${PORT}`)
+);
