ad3c467b235a39036c05c5e93ab9f4ad583cf714
Tejas Nikhar <tejastn10@gmail.com>
2020-12-23 16:15:08 +0530

    📦 NEW: Basic Layout

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,5 +1,15 @@
+import { Layout } from "antd";
+
+import "./App.css";
+
 const App = () => {
-  return <div>Shoe Shoppee</div>;
+  return (
+    <Layout className="bg">
+      <Layout.Header className="hdr"></Layout.Header>
+      <Layout.Content className="cnt"></Layout.Content>
+      <Layout.Footer className="ftr"></Layout.Footer>
+    </Layout>
+  );
 };
 
 export default App;
