16030d71329c0a4bcfe5728ff32a3c2855864a88
Tejas Nikhar <tejastn10@gmail.com>
2020-12-24 19:09:45 +0530

    🛣️ UPDATE: Routes File

--- frontend/web/src/routes/Routes.tsx
+++ frontend/web/src/routes/Routes.tsx
@@ -1,8 +1,12 @@
+import { LogIn } from "../components/auth/LogIn";
+import { Register } from "../components/auth/Register";
 import { Landing } from "../components/Landing";
 
 export const Routes = () => {
   return (
     <>
+      <LogIn />
+      <Register />
       <Landing />
     </>
   );
