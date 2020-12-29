e45a98a07dd57e8f2721916d18d3a6b3c85e3781
Tejas Nikhar <tejastn10@gmail.com>
2020-12-29 19:21:25 +0530

    🧱 NEW: User Model

---
+++ backend/src/models/User.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "../@types/User";
+
+const userSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model<IUser>("User", userSchema);
