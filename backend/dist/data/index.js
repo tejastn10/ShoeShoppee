b394c187fee4d73f5ba53d58cb83037780c7b61a
Tejas Nikhar <tejastn10@gmail.com>
2020-12-30 16:31:55 +0530

    📦 NEW: Index File for Dummy Data

---
+++ backend/dist/data/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.users = exports.products = void 0;
+const products_1 = require("./products");
+Object.defineProperty(exports, "products", { enumerable: true, get: function () { return products_1.products; } });
+const users_1 = require("./users");
+Object.defineProperty(exports, "users", { enumerable: true, get: function () { return users_1.users; } });
---
+++ backend/src/data/index.ts
@@ -0,0 +1,4 @@
+import { products } from "./products";
+import { users } from "./users";
+
+export { products, users };
