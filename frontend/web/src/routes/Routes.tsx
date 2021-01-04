64823c0e325718b795a9e1a038b8b3dd4c90e52b
Tejas Nikhar <tejastn10@gmail.com>
2021-01-04 12:54:21 +0530

    🛣️ UPDATE: Routes File

--- frontend/web/src/routes/Routes.tsx
+++ frontend/web/src/routes/Routes.tsx
@@ -1,3 +1,5 @@
+import { Route, Switch } from "react-router-dom";
+
 import { LogIn } from "../components/auth/LogIn";
 import { Register } from "../components/auth/Register";
 import { Landing } from "../components/Landing";
@@ -6,10 +8,12 @@ import { Search } from "../containers/Search";
 export const Routes = () => {
   return (
     <>
-      <LogIn />
-      <Register />
-      <Landing />
-      <Search />
+      <Route exact path="/" component={Landing} />
+      <Switch>
+        <Route exact path="/login" component={LogIn} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/search/*" component={Search} />
+      </Switch>
     </>
   );
 };
