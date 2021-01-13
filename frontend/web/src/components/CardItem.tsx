43f15c569de460a131d65b2d2577f3be698f2226
Tejas Nikhar <tejastn10@gmail.com>
2021-01-13 22:23:49 +0530

    📦 UPDATE: CardItem Component

--- frontend/web/src/components/CardItem.tsx
+++ frontend/web/src/components/CardItem.tsx
@@ -1,25 +1,27 @@
 import { Col, Card } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
+import { Product } from "../store/@types";
+import { Rating } from "./Rating";
 
-export const CardItem = () => {
+type CardProps = {
+  product: Product;
+};
+
+export const CardItem = ({ product }: CardProps) => {
   return (
-    <div>
-      <Col className="gutter-row" span={6}>
+    <Col className="gutter-row" span={6}>
+      <Link to={`/product/${product._id}`}>
         <Card
+          className="card"
           hoverable
-          cover={
-            <img
-              alt="example"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-            />
-          }
+          cover={<img alt="example" src={product.image} />}
         >
-          <Card.Meta
-            title="Europe Street beat"
-            description="www.instagram.com"
-          />
+          <Card.Meta title={product.name} description="Rating" />
+          <Rating rating={product.rating} reviews={product.numReviews} />
+          <h2>₹ {product.price}</h2>
         </Card>
-      </Col>
-    </div>
+      </Link>
+    </Col>
   );
 };
