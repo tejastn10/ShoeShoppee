951dc06adf48240525405e714664de7c27c98a90
Tejas Nikhar <tejastn10@gmail.com>
2021-01-15 21:21:45 +0530

    🔗 UPDATE: Root Reducer

--- frontend/web/src/store/reducers/reducer.ts
+++ frontend/web/src/store/reducers/reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { ProductListStatereducer } from "./productList";
+import { ProductDetailsStateReducer } from "./productDetails";
 
 const reducers = {
   productList: ProductListStatereducer,
+  productDetails: ProductDetailsStateReducer,
 };
 
 export const rootReducer = () => {
