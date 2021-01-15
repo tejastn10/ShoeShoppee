27cd66e6cd910d9da40e0c9ffd44304d8cb85604
Tejas Nikhar <tejastn10@gmail.com>
2021-01-16 01:45:11 +0530

    🎛️ NEW: User Controller

---
+++ backend/src/controllers/user.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+
+import { User } from "./../models/User.model";
+
+export const postUser = async (req: Request, res: Response) => {
+  const { email, password } = await req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Email or Password!");
+  }
+};
