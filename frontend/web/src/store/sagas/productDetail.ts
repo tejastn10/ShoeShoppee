0b550f20ac5f2f0fc446a788aa4e5776de654a1f
Tejas Nikhar <tejastn10@gmail.com>
2021-01-15 21:22:18 +0530

    🧩 NEW: ProductDetails Saga

---
+++ frontend/web/src/store/sagas/productDetail.ts
@@ -0,0 +1,36 @@
+import { Action } from "@reduxjs/toolkit";
+import { all, call, fork, put, takeLatest } from "redux-saga/effects";
+
+import * as API from "../../services/api";
+
+import { getCustomError } from "../../utils/api-helper";
+
+import {
+  getProductRequest,
+  getProductSuccess,
+  getProductError,
+} from "../actions/actions";
+
+const getProductDetails = function* (action: Action) {
+  try {
+    if (getProductRequest.match(action)) {
+      const res = yield call(API.fetchProductDetails, action.payload);
+      const data = res.data;
+      if (res.status !== 200) {
+        yield put(getProductError(data.error));
+      } else {
+        yield put(getProductSuccess(data));
+      }
+    }
+  } catch (error) {
+    yield put(getProductError(getCustomError(error)));
+  }
+};
+
+const watchProductDetailsRequest = function* () {
+  yield takeLatest(getProductRequest.type, getProductDetails);
+};
+
+export default function* productDetailsSaga() {
+  yield all([fork(watchProductDetailsRequest)]);
+}
