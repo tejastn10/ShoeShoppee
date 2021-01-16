8a4b2d7628d575066ccb7c852585efda2b381a82
Tejas Nikhar <tejastn10@gmail.com>
2021-01-16 22:03:57 +0530

    🧩 NEW: Auth Middle-ware

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { User } from "../models";
+
+type decoded = {
+  id: string;
+};
+
+export const protect = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = <decoded>verify(token, process.env.JWT_SECRET!);
+
+      req.body.user = await User.findById(decoded.id).select("-password");
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not Authorized! Token Failed ❌");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not Authorized! Token not found");
+  }
+
+  next();
+};
