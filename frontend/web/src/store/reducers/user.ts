e3c30533c78d732c20d7c3c41ddcf46d2ba72019
Tejas Nikhar <tejastn10@gmail.com>
2021-01-17 12:10:37 +0530

    🔗 NEW: UserState Reducer

---
+++ frontend/web/src/store/reducers/user.ts
@@ -0,0 +1,49 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  userLoginRequest,
+  userLoginSuccess,
+  userLoginError,
+  userLogoutRequest,
+} from "../actions/actions";
+import { UserState } from "../@types";
+import {
+  clearFromLocalStorage,
+  saveToLocalStorage,
+} from "../../utils/api-helper";
+
+const initialState: UserState = {
+  isLoading: false,
+  errors: {
+    results: null,
+  },
+  user: null,
+};
+
+const reducer = createReducer(initialState, (builder) => {
+  return builder
+    .addCase(userLoginRequest, (state, _action) => {
+      state.isLoading = true;
+      state.errors.results = null;
+      state.user = null;
+    })
+    .addCase(userLoginSuccess, (state, action) => {
+      saveToLocalStorage("user", action.payload);
+      state.isLoading = false;
+      state.user = action.payload;
+    })
+    .addCase(userLoginError, (state, action) => {
+      clearFromLocalStorage("user");
+      state.isLoading = false;
+      state.errors.results = action.payload;
+    })
+    .addCase(userLogoutRequest, (state, _action) => {
+      clearFromLocalStorage("user");
+      state.isLoading = false;
+      state.user = null;
+    })
+    .addDefaultCase((state, _action) => {
+      state.isLoading = false;
+    });
+});
+
+export { initialState as UserInitialState, reducer as UserStateReducer };
