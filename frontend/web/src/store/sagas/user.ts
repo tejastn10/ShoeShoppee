2506b875fdbbeca72cf231dbc7e2083f98607298
Tejas Nikhar <tejastn10@gmail.com>
2021-01-17 12:11:46 +0530

    🧩 NEW: User Saga

---
+++ frontend/web/src/store/sagas/user.ts
@@ -0,0 +1,37 @@
+import { Action } from "@reduxjs/toolkit";
+import { all, call, fork, put, takeLatest } from "redux-saga/effects";
+
+import * as API from "../../services/api";
+
+import { getCustomError } from "../../utils/api-helper";
+
+import {
+  userLoginRequest,
+  userLoginSuccess,
+  userLoginError,
+} from "../actions/actions";
+
+const getUser = function* (action: Action) {
+  try {
+    if (userLoginRequest.match(action)) {
+      const res = yield call(API.fetchUser, action.payload);
+      const data = res.data;
+      if (res.status !== 200) {
+        console.log(res.data.error.message);
+        yield put(userLoginError(data.error));
+      } else {
+        yield put(userLoginSuccess(data));
+      }
+    }
+  } catch (error) {
+    yield put(userLoginError(getCustomError(error.response.data)));
+  }
+};
+
+const watchUserRequest = function* () {
+  yield takeLatest(userLoginRequest.type, getUser);
+};
+
+export default function* userSaga() {
+  yield all([fork(watchUserRequest)]);
+}
