47c5c07e715899d4372376b01acc788b481fc5e0
Tejas Nikhar <tejastn10@gmail.com>
2021-01-18 18:27:48 +0530

    🔗 NEW: Auth Reducer

---
+++ frontend/web/src/store/reducers/auth.ts
@@ -0,0 +1,68 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  loginAuthRequest,
+  loginAuthSuccess,
+  loginAuthError,
+  logoutUser,
+  registerAuthRequest,
+  registerAuthSuccess,
+  registerAuthError,
+} from "../actions/actions";
+import { AuthState } from "../@types";
+import {
+  clearFromLocalStorage,
+  getFromLocalStorage,
+  saveToLocalStorage,
+} from "../../utils/api-helper";
+
+const initialState: AuthState = {
+  isLoading: false,
+  errors: {
+    results: null,
+  },
+  auth: getFromLocalStorage("user"),
+};
+
+const reducer = createReducer(initialState, (builder) => {
+  return builder
+    .addCase(loginAuthRequest, (state, _action) => {
+      state.isLoading = true;
+      state.errors.results = null;
+      state.auth = null;
+    })
+    .addCase(loginAuthSuccess, (state, action) => {
+      saveToLocalStorage("user", action.payload);
+      state.isLoading = false;
+      state.auth = action.payload;
+    })
+    .addCase(loginAuthError, (state, action) => {
+      clearFromLocalStorage("user");
+      state.isLoading = false;
+      state.errors.results = action.payload;
+    })
+    .addCase(registerAuthRequest, (state, _action) => {
+      state.isLoading = true;
+      state.errors.results = null;
+      state.auth = null;
+    })
+    .addCase(registerAuthSuccess, (state, action) => {
+      saveToLocalStorage("user", action.payload);
+      state.isLoading = false;
+      state.auth = action.payload;
+    })
+    .addCase(registerAuthError, (state, action) => {
+      clearFromLocalStorage("user");
+      state.isLoading = false;
+      state.errors.results = action.payload;
+    })
+    .addCase(logoutUser, (state, _action) => {
+      clearFromLocalStorage("user");
+      state.isLoading = false;
+      state.auth = null;
+    })
+    .addDefaultCase((state, _action) => {
+      state.isLoading = false;
+    });
+});
+
+export { initialState as AuthInitialState, reducer as AuthStateReducer };
