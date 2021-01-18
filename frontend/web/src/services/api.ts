07af7f1581133ee2eb145f85b1bf234593901517
Tejas Nikhar <tejastn10@gmail.com>
2021-01-18 18:20:21 +0530

    🔇 LOG: User Data

--- frontend/web/src/services/api.ts
+++ frontend/web/src/services/api.ts
@@ -27,7 +27,6 @@ export const loginUser: any = ({ email, password }: any) => {
 
 export const registerUser: any = ({ name, email, password }: any) => {
   const URL = `${API_ENDPOINT}/api/users`;
-  console.log({ name, email, password });
 
   return API.post(URL, { name, email, password });
 };
