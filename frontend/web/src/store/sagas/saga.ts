4b538466aaa85bfa3682a612d70b4ae407afd607
Tejas Nikhar <tejastn10@gmail.com>
2021-01-18 20:05:05 +0530

    🧩 UPDATE: Root Saga

--- frontend/web/src/store/sagas/saga.ts
+++ frontend/web/src/store/sagas/saga.ts
@@ -2,7 +2,13 @@ import { all, fork } from "redux-saga/effects";
 import productListSaga from "./productList";
 import productSaga from "./productDetail";
 import authSaga from "./auth";
+import userSaga from "./user";
 
 export const rootSaga = function* () {
-  yield all([fork(productListSaga), fork(productSaga), fork(authSaga)]);
+  yield all([
+    fork(productListSaga),
+    fork(productSaga),
+    fork(authSaga),
+    fork(userSaga),
+  ]);
 };
