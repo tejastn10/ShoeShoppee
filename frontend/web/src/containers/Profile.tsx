14973d3e5d8a9237a6222106c7dad0d9c8378227
Tejas Nikhar <tejastn10@gmail.com>
2021-01-18 20:08:51 +0530

    📦 NEW: Profile Container

---
+++ frontend/web/src/containers/Profile.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { ApplicationState } from "../store/store";
+import { AuthState, UserProfileState } from "../store/@types";
+import { message } from "antd";
+import { getUserProfileRequest } from "../store/actions/actions";
+
+export const Profile = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const authState = useSelector<ApplicationState, AuthState>(
+    (state) => state.authState
+  );
+  const profileState = useSelector<ApplicationState, UserProfileState>(
+    (state) => state.userProfile
+  );
+
+  const { auth } = authState;
+  const { errors, profile } = profileState;
+
+  useEffect(() => {
+    if (errors.results) {
+      message.error(errors.results.message);
+    }
+  }, [errors.results]);
+
+  useEffect(() => {
+    if (!auth) {
+      history.push("/login");
+    } else {
+      if (!profile) {
+        dispatch(getUserProfileRequest());
+      }
+    }
+  }, [history, auth, dispatch, profile]);
+
+  return <div>Profile</div>;
+};
