3ee67a8eee97b953cb9a014a49550c2e5739ca14
Tejas Nikhar <tejastn10@gmail.com>
2021-01-18 20:04:04 +0530

    🔗 NEW: User Profile Reducer

---
+++ frontend/web/src/store/reducers/user.ts
@@ -0,0 +1,45 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  getUserProfileRequest,
+  getUserProfileSuccess,
+  getUserProfileError,
+  clearUserProfile,
+} from "../actions/actions";
+import { UserProfileState } from "../@types";
+
+const initialState: UserProfileState = {
+  isLoading: false,
+  errors: {
+    results: null,
+  },
+  profile: null,
+};
+
+const reducer = createReducer(initialState, (builder) => {
+  return builder
+    .addCase(getUserProfileRequest, (state, _action) => {
+      state.isLoading = true;
+      state.errors.results = null;
+      state.profile = null;
+    })
+    .addCase(getUserProfileSuccess, (state, action) => {
+      state.isLoading = false;
+      state.profile = action.payload;
+    })
+    .addCase(getUserProfileError, (state, action) => {
+      state.isLoading = false;
+      state.errors.results = action.payload;
+    })
+    .addCase(clearUserProfile, (state, _action) => {
+      state.isLoading = false;
+      state.profile = null;
+    })
+    .addDefaultCase((state, _action) => {
+      state.isLoading = false;
+    });
+});
+
+export {
+  initialState as UserProfileInitialState,
+  reducer as UserProfileStateReducer,
+};
