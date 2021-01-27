e5388882fd88eada65ea2d5fc132ff349d7a0503
Tejas Nikhar <tejastn10@gmail.com>
2021-01-27 23:03:13 +0530

    📦 NEW: Payment Container

---
+++ frontend/web/src/containers/Payment.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Card, Radio, Button, message } from "antd";
+import { RadioChangeEvent } from "antd/lib/radio";
+
+import { ApplicationState } from "../store/store";
+import { CartState } from "../store/@types";
+import { savePaymentMethod } from "../store/actions/cart";
+
+export const Payment = ({ prev, next }: any) => {
+  const cartState = useSelector<ApplicationState, CartState>(
+    (state) => state.cart
+  );
+  const dispatch = useDispatch();
+
+  const { paymentMethod } = cartState;
+  const [payment, setPayment] = useState(paymentMethod);
+
+  const onChange = (e: RadioChangeEvent) => {
+    console.log("radio checked", e.target.value);
+    setPayment(e.target.value);
+  };
+
+  const onSubmit = () => {
+    message.warning("Please check your details before confirming order!");
+    dispatch(savePaymentMethod(payment));
+    next();
+  };
+
+  return (
+    <Card className="shipping" title="Select Payment Method">
+      <Radio.Group onChange={onChange} value={payment} className="payment">
+        <Radio value={"PayPal"}>PayPal or Card</Radio>
+        <Radio value={"UPI"}>UPI</Radio>
+        <Radio value={"Cash"}>Cash</Radio>
+      </Radio.Group>
+      <Button type="primary" onClick={onSubmit}>
+        Next
+      </Button>
+      <Button style={{ margin: "0 8px" }} onClick={prev}>
+        Previous
+      </Button>
+    </Card>
+  );
+};
