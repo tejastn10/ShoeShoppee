cee3fd1a83f72d84bb368d62e733f8a866251e76
Tejas Nikhar <tejastn10@gmail.com>
2021-01-27 22:06:10 +0530

    📦 NEW: Checkout Container

---
+++ frontend/web/src/containers/Checkout.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import { Card, PageHeader, Steps } from "antd";
+
+import { Shipping } from "./Shipping";
+import { Payment } from "./Payment";
+
+const { Step } = Steps;
+
+export const Checkout = () => {
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const steps = [
+    { title: "Shipping Address", content: <Shipping next={next} /> },
+    { title: "Payment", content: <Payment prev={prev} /> },
+  ];
+
+  return (
+    <>
+      <Card>
+        <PageHeader>
+          <Steps current={current} progressDot>
+            {steps.map((item) => (
+              <Step key={item.title} title={item.title} />
+            ))}
+          </Steps>
+        </PageHeader>
+      </Card>
+      <Card>
+        <div className="steps-content">{steps[current].content}</div>
+      </Card>
+    </>
+  );
+};
