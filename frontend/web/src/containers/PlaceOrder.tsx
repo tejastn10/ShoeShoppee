52ee04927c16a6154cd862051eca37e5208adc2f
Tejas Nikhar <tejastn10@gmail.com>
2021-01-31 15:01:10 +0530

    📦 UPDATE: PlaceOrder Container

--- frontend/web/src/containers/PlaceOrder.tsx
+++ frontend/web/src/containers/PlaceOrder.tsx
@@ -26,6 +26,7 @@ type PlaceOrderProps = {
 
 export const PlaceOrder = ({ prev }: PlaceOrderProps) => {
   const cart = useSelector<ApplicationState, CartState>((state) => state.cart);
+  const { totalItems, shippingAddress, paymentMethod, cartList, price } = cart;
 
   return (
     <Card>
@@ -50,16 +51,16 @@ export const PlaceOrder = ({ prev }: PlaceOrderProps) => {
         <Divider orientation="left">Shipping Address</Divider>
         <Descriptions size="small" column={1}>
           <Descriptions.Item label="Address">
-            {cart.shippingAddress?.address}
+            {shippingAddress!.address}
           </Descriptions.Item>
           <Descriptions.Item label="City">
-            {cart.shippingAddress?.city}
+            {shippingAddress!.city}
           </Descriptions.Item>
           <Descriptions.Item label="PinCode">
-            {cart.shippingAddress?.pincode}
+            {shippingAddress!.pincode}
           </Descriptions.Item>
           <Descriptions.Item label="State">
-            {cart.shippingAddress?.state}
+            {shippingAddress!.state}
           </Descriptions.Item>
         </Descriptions>
         <Divider orientation="left">Payment</Divider>
@@ -68,20 +69,20 @@ export const PlaceOrder = ({ prev }: PlaceOrderProps) => {
             <Statistic title="Status" value="Pending" />
           </Descriptions.Item>
           <Descriptions.Item>
-            <Statistic title="Payment Method" value={cart.paymentMethod} />
+            <Statistic title="Payment Method" value={paymentMethod} />
           </Descriptions.Item>
         </Descriptions>
         <Divider />
         <Row>
           <Col span={18}>
             <Card title="List Items" bordered={false}>
-              {cart.cartList!.map((item) => {
+              {cartList!.map((item) => {
                 return <OrderItem item={item} key={item.id} />;
               })}
             </Card>
           </Col>
           <Col span={6}>
-            <CartSummary list={cart.cartList!} />
+            <CartSummary totalItems={totalItems} price={price!} />
           </Col>
         </Row>
       </PageHeader>
