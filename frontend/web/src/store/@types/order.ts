91cfbab209449d40f7e6621b0707dd20081f9004
Tejas Nikhar <tejastn10@gmail.com>
2021-02-01 14:59:49 +0530

    🏷 ️UPDATE: Order and OrderAction Types

--- frontend/web/src/store/@types/order.ts
+++ frontend/web/src/store/@types/order.ts
@@ -1,6 +1,19 @@
+import { CartItem, ShippingAddress } from ".";
 import { CustomError } from "../../utils/api-helper";
 
-export interface Order {}
+export interface Order {
+  _id: string;
+  shippingAddress: ShippingAddress;
+  paymentMethod: "PayPal" | "UPI" | "Cash";
+  orderItems: CartItem[];
+  totalItems: number;
+  itemsPrice: number;
+  taxPrice: number;
+  shippingPrice: number;
+  totalPrice: number;
+  isPaid: boolean;
+  isDelivered: boolean;
+}
 
 export interface Errors {
   results: CustomError | null;
@@ -10,6 +23,7 @@ export interface OrderState {
   isLoading: boolean;
   success: boolean | null;
   errors: Errors;
+  listError: Errors;
   orders: Order[] | null;
 }
 
@@ -18,4 +32,8 @@ export enum OrderActionTypes {
   CREATE_ORDER_SUCCESS = "@@order/CREATE_ORDER_SUCCESS",
   CREATE_ORDER_ERROR = "@@order/CREATE_ORDER_ERROR",
   RESET_ORDER = "@@order/RESET_ORDER",
+  ORDER_LIST_REQUEST = "@@order/ORDER_LIST_REQUEST",
+  ORDER_LIST_SUCCESS = "@@order/ORDER_LIST_SUCCESS",
+  ORDER_LIST_ERROR = "@@order/ORDER_LIST_ERROR",
+  CLEAR_ORDERS = "@@order/CLEAR_ORDERS",
 }
