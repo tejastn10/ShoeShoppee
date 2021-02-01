e97dbb7a073a9b504ae96fa1620d53b589e742f1
Tejas Nikhar <tejastn10@gmail.com>
2021-02-01 15:01:56 +0530

    🧩 UPDATE: Order Saga

--- frontend/web/src/store/sagas/order.ts
+++ frontend/web/src/store/sagas/order.ts
@@ -9,6 +9,9 @@ import {
   createOrderRequest,
   createOrderSuccess,
   createOrderError,
+  ordersListRequest,
+  ordersListSuccess,
+  ordersListError,
 } from "../actions/actions";
 
 const createOrder = function* (action: Action) {
@@ -27,10 +30,30 @@ const createOrder = function* (action: Action) {
   }
 };
 
+const orderList = function* (action: Action) {
+  try {
+    if (ordersListRequest.match(action)) {
+      const res = yield call(API.getOrderList, action.payload);
+      const data = res.data;
+      if (res.status !== 200) {
+        yield put(ordersListError(data.error));
+      } else {
+        yield put(ordersListSuccess(data));
+      }
+    }
+  } catch (error) {
+    yield put(ordersListError(getCustomError(error.response.data)));
+  }
+};
+
 const watchCreateOrderRequest = function* () {
   yield takeLatest(createOrderRequest.type, createOrder);
 };
 
+const watchOrderListRequest = function* () {
+  yield takeLatest(ordersListRequest.type, orderList);
+};
+
 export default function* orderSaga() {
-  yield all([fork(watchCreateOrderRequest)]);
+  yield all([fork(watchCreateOrderRequest), fork(watchOrderListRequest)]);
 }
