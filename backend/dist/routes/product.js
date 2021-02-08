135f88a6e60e1d7b8d09677f0ae0be8ff00aa16f
Tejas Nikhar <tejastn10@gmail.com>
2021-02-08 16:10:08 +0530

    🛣️ UPDATE: Product Routes

--- backend/dist/routes/product.js
+++ backend/dist/routes/product.js
@@ -7,6 +7,7 @@ exports.router = void 0;
 const express_1 = require("express");
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
 const product_1 = require("../controllers/product");
+const auth_1 = require("../middleware/auth");
 exports.router = express_1.Router();
 // @desc     Fetch all Products
 // @route    GET /api/products
@@ -16,3 +17,15 @@ exports.router.get("/", express_async_handler_1.default(product_1.getAllPoducts)
 // @route    GET /api/products/:id
 // @access   Public
 exports.router.get("/:id", express_async_handler_1.default(product_1.getProductById));
+// @desc     Create a Product
+// @route    POST /api/products
+// @access   Private/Admin
+exports.router.post("/", express_async_handler_1.default(auth_1.protect), express_async_handler_1.default(auth_1.admin), express_async_handler_1.default(product_1.postProduct));
+// @desc     Update a Product
+// @route    POST /api/products/:id
+// @access   Private/Admin
+exports.router.put("/:id", express_async_handler_1.default(auth_1.protect), express_async_handler_1.default(auth_1.admin), express_async_handler_1.default(product_1.putProduct));
+// @desc     Delete a Product
+// @route    DELETE /api/products/:id
+// @access   Private/Admin
+exports.router.delete("/:id", express_async_handler_1.default(auth_1.protect), express_async_handler_1.default(auth_1.admin), express_async_handler_1.default(product_1.deleteProduct));
--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,6 +1,13 @@
 import { Router } from "express";
 import asyncHandler from "express-async-handler";
-import { getAllPoducts, getProductById } from "../controllers/product";
+import {
+  getAllPoducts,
+  getProductById,
+  postProduct,
+  putProduct,
+  deleteProduct,
+} from "../controllers/product";
+import { protect, admin } from "../middleware/auth";
 
 export const router: Router = Router();
 
@@ -13,3 +20,33 @@ router.get("/", asyncHandler(getAllPoducts));
 // @route    GET /api/products/:id
 // @access   Public
 router.get("/:id", asyncHandler(getProductById));
+
+// @desc     Create a Product
+// @route    POST /api/products
+// @access   Private/Admin
+router.post(
+  "/",
+  asyncHandler(protect),
+  asyncHandler(admin),
+  asyncHandler(postProduct)
+);
+
+// @desc     Update a Product
+// @route    POST /api/products/:id
+// @access   Private/Admin
+router.put(
+  "/:id",
+  asyncHandler(protect),
+  asyncHandler(admin),
+  asyncHandler(putProduct)
+);
+
+// @desc     Delete a Product
+// @route    DELETE /api/products/:id
+// @access   Private/Admin
+router.delete(
+  "/:id",
+  asyncHandler(protect),
+  asyncHandler(admin),
+  asyncHandler(deleteProduct)
+);
