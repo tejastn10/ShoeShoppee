6794efd3460800caf07c245d4523729eecc03790
Tejas Nikhar <tejastn10@gmail.com>
2021-02-08 16:09:41 +0530

    🎛️ UPDATE: Product Controller

--- backend/dist/controllers/product.js
+++ backend/dist/controllers/product.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProductById = exports.getAllPoducts = void 0;
+exports.deleteProduct = exports.putProduct = exports.postProduct = exports.getProductById = exports.getAllPoducts = void 0;
 const models_1 = require("./../models");
 const getAllPoducts = (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const products = yield models_1.Product.find({});
@@ -27,3 +27,47 @@ const getProductById = (req, res) => __awaiter(void 0, void 0, void 0, function*
     }
 });
 exports.getProductById = getProductById;
+const postProduct = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const product = new models_1.Product({
+        user: req.body.user._id,
+        name: "sample",
+        image: "/images/file.jpg",
+        brand: "sample",
+        category: "sample",
+        type: "sample",
+        description: "sample",
+        reviews: [],
+        price: 1000,
+        count: 10,
+    });
+    const createdProduct = yield product.save();
+    res.status(201).json(createdProduct);
+});
+exports.postProduct = postProduct;
+const putProduct = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const product = yield models_1.Product.findById(req.params.id);
+    const { count, price } = req.body;
+    if (product) {
+        product.count = count ? count : product.count;
+        product.price = price ? price : product.price;
+        yield product.save();
+        res.json({ message: "Product Updated" });
+    }
+    else {
+        res.status(404);
+        throw new Error("Product Not found");
+    }
+});
+exports.putProduct = putProduct;
+const deleteProduct = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const product = yield models_1.Product.findById(req.params.id);
+    if (product) {
+        yield product.remove();
+        res.json({ message: "Product deleted" });
+    }
+    else {
+        res.status(404);
+        throw new Error("Product Not found");
+    }
+});
+exports.deleteProduct = deleteProduct;
--- backend/src/controllers/product.ts
+++ backend/src/controllers/product.ts
@@ -16,3 +16,48 @@ export const getProductById = async (req: Request, res: Response) => {
     throw new Error("Product Not found");
   }
 };
+
+export const postProduct = async (req: Request, res: Response) => {
+  const product = new Product({
+    user: req.body.user._id,
+    name: "sample",
+    image: "/images/file.jpg",
+    brand: "sample",
+    category: "sample",
+    type: "sample",
+    description: "sample",
+    reviews: [],
+    price: 1000,
+    count: 10,
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+};
+
+export const putProduct = async (req: Request, res: Response) => {
+  const product = await Product.findById(req.params.id);
+  const { count, price } = req.body;
+
+  if (product) {
+    product.count = count ? count : product.count;
+    product.price = price ? price : product.price;
+    await product.save();
+    res.json({ message: "Product Updated" });
+  } else {
+    res.status(404);
+    throw new Error("Product Not found");
+  }
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product deleted" });
+  } else {
+    res.status(404);
+    throw new Error("Product Not found");
+  }
+};
