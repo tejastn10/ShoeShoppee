4f286bd95c1e0e860ac8830cd1f9b022b90e378b
Tejas Nikhar <tejastn10@gmail.com>
2021-02-22 14:12:20 +0530

    📦 NEW: SearchBox Container

---
+++ frontend/web/src/containers/product/SearchBox.tsx
@@ -0,0 +1,31 @@
+import { Input, message } from "antd";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { searchProductRequest } from "../../store/actions/actions";
+
+const { Search } = Input;
+
+export const SearchBox = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const onSearch = (value: string) => {
+    if (value) {
+      const keyword = value.trim();
+      dispatch(searchProductRequest({ keyword }));
+      history.push(`/search/${keyword}`);
+    } else {
+      message.warning("Searchfield empty");
+      history.push(`/search`);
+    }
+  };
+
+  return (
+    <Search
+      className="search"
+      allowClear
+      placeholder="Search your favourite shoes"
+      enterButton
+      onSearch={(value) => onSearch(value)}
+    />
+  );
+};
