7c00b68ea16f623b88125734867c92a54d068e6a
Tejas Nikhar <tejastn10@gmail.com>
2021-02-22 15:20:56 +0530

    📦 UPDATE: Search Container with Pagination

--- frontend/web/src/containers/product/Search.tsx
+++ frontend/web/src/containers/product/Search.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
-import { Empty, message, PageHeader, Row, Card } from "antd";
+import { Empty, message, PageHeader, Pagination, Row, Card } from "antd";
 
-import { ProductListState } from "../../store/@types";
+import { Product, ProductListState } from "../../store/@types";
 
 import {
   getProductListRequest,
@@ -22,6 +22,10 @@ export const Search = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { keyword }: SearchParams = useParams();
+
+  const [offset, setOffset] = useState(0);
+  const [pageItems, setPageItems] = useState<Product[] | null>(null);
+  const [totalItemsCount, setTotalItemsCount] = useState(0);
 
   const productList = useSelector<ApplicationState, ProductListState>(
     (state) => state.productList
@@ -42,6 +46,23 @@ export const Search = () => {
     }
   }, [errors.results]);
 
+  useEffect(() => {
+    if (products) {
+      setTotalItemsCount(products.length);
+    }
+  }, [products]);
+
+  useEffect(() => {
+    if (products) {
+      setPageItems(products!.slice(offset, offset + 12));
+    }
+  }, [offset, products]);
+
+  const onPageChange = (page: number) => {
+    console.log(page);
+    setOffset((page - 1) * 12);
+  };
+
   return (
     <div className="container">
       <Card>
@@ -60,11 +81,19 @@ export const Search = () => {
             <Empty />
           </div>
         ) : (
-          <Row gutter={6}>
-            {products?.map((p) => (
-              <CardItem product={p} key={p._id} />
-            ))}
-          </Row>
+          <>
+            <Row gutter={6}>
+              {pageItems?.map((p) => (
+                <CardItem product={p} key={p._id} />
+              ))}
+            </Row>
+            <Pagination
+              style={{ display: "flex", justifyContent: "center" }}
+              onChange={onPageChange}
+              defaultCurrent={1}
+              total={totalItemsCount}
+            />
+          </>
         )}
       </Card>
     </div>
