2e3fd2bfc0e464e330fe2f6e0d1589e392f99a20
Tejas Nikhar <tejastn10@gmail.com>
2021-02-22 10:33:22 +0530

    🧱 UPDATE: Product Model

--- backend/dist/models/Product.model.js
+++ backend/dist/models/Product.model.js
@@ -3,6 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Product = void 0;
 const mongoose_1 = require("mongoose");
 const reviewSchema = new mongoose_1.Schema({
+    user: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
     name: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
