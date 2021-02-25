89674a56394cf2e7d6f946df684d7ffbdf1fa4df
Tejas Nikhar <tejastn10@gmail.com>
2021-02-25 14:04:52 +0530

    🧪 UPDATE: Test

--- frontend/web/src/App.test.tsx
+++ frontend/web/src/App.test.tsx
@@ -1,9 +1,3 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import "@testing-library/react";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("Renders React App", () => {});
