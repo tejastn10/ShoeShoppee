6f638a7aad4fac067f5d35f9b1d0d2d1108c3f64
Tejas Nikhar <tejastn10@gmail.com>
2021-02-25 14:25:31 +0530

    💾 UPDATE: Dummy User Data

--- backend/dist/data/users.js
+++ backend/dist/data/users.js
@@ -4,26 +4,44 @@ exports.users = void 0;
 const bcryptjs_1 = require("bcryptjs");
 exports.users = [
     {
-        name: "Admin",
+        name: "Main Admin",
         email: "admin@admin.com",
         password: bcryptjs_1.hashSync("123456", 10),
         isAdmin: true,
     },
     {
-        name: "Cool Boi",
-        email: "cb@email.com",
+        name: "Side Admin",
+        email: "side@admin.com",
+        password: bcryptjs_1.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name: "Micheal Scott",
+        email: "mscott28@gmail.com",
+        password: bcryptjs_1.hashSync("123456", 10),
+        isAdmin: true,
+    },
+    {
+        name: "Ollie Bashirian",
+        email: "ollbash4@yahoo.com",
         password: bcryptjs_1.hashSync("123456", 10),
         isAdmin: false,
     },
     {
-        name: "Bad Boi",
-        email: "bb@email.com",
+        name: "Leola Carter",
+        email: "leocarter3@gmail.com",
         password: bcryptjs_1.hashSync("123456", 10),
         isAdmin: false,
     },
     {
-        name: "Mean Girl",
-        email: "mg@email.com",
+        name: "Haley Lehner",
+        email: "haley_lehner@hotmail.com",
+        password: bcryptjs_1.hashSync("123456", 10),
+        isAdmin: false,
+    },
+    {
+        name: "Calista Cummings",
+        email: "calista71@yahoo.com",
         password: bcryptjs_1.hashSync("123456", 10),
         isAdmin: false,
     },
--- backend/src/data/users.ts
+++ backend/src/data/users.ts
@@ -2,26 +2,44 @@ import { hashSync } from "bcryptjs";
 
 export const users = [
   {
-    name: "Admin",
+    name: "Main Admin",
     email: "admin@admin.com",
     password: hashSync("123456", 10),
     isAdmin: true,
   },
   {
-    name: "Cool Boi",
-    email: "cb@email.com",
+    name: "Side Admin",
+    email: "side@admin.com",
+    password: hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Micheal Scott",
+    email: "mscott28@gmail.com",
+    password: hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Ollie Bashirian",
+    email: "ollbash4@yahoo.com",
     password: hashSync("123456", 10),
     isAdmin: false,
   },
   {
-    name: "Bad Boi",
-    email: "bb@email.com",
+    name: "Leola Carter",
+    email: "leocarter3@gmail.com",
     password: hashSync("123456", 10),
     isAdmin: false,
   },
   {
-    name: "Mean Girl",
-    email: "mg@email.com",
+    name: "Haley Lehner",
+    email: "haley_lehner@hotmail.com",
+    password: hashSync("123456", 10),
+    isAdmin: false,
+  },
+  {
+    name: "Calista Cummings",
+    email: "calista71@yahoo.com",
     password: hashSync("123456", 10),
     isAdmin: false,
   },
