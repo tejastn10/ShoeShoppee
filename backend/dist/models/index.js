1a7cfb14eaf028b72124882b0d7633d36f6ccf83
Tejas Nikhar <tejastn10@gmail.com>
2021-03-15 21:19:32 +0530

    🗑 REMOVE: Dist Folder

--- backend/dist/models/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.User = exports.Product = exports.Order = void 0;
-const Order_model_1 = require("./Order.model");
-Object.defineProperty(exports, "Order", { enumerable: true, get: function () { return Order_model_1.Order; } });
-const Product_model_1 = require("./Product.model");
-Object.defineProperty(exports, "Product", { enumerable: true, get: function () { return Product_model_1.Product; } });
-const User_model_1 = require("./User.model");
-Object.defineProperty(exports, "User", { enumerable: true, get: function () { return User_model_1.User; } });
